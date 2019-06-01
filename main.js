0c2f86cf5996d3d5db8b93d839cbbdea9bb6fb37
Matteo Sacchi <matteoseik@hotmail.it>
2019-06-01 17:54:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -308,7 +308,7 @@ window.addEventListener('deviceorientation', function(event) {
     if (window.orientation === 0) {
     	// The device is right-side up in portrait orientation
     	motion.x = event.gamma - motion_initial.y;
-    	motion.y = event.beta + motion_initial.x;
+    	motion.y = event.beta - motion_initial.x;
     } else if (window.orientation === 90) {
     	// The device is in landscape laying on its left side
     	motion.x = event.beta - motion_initial.x;
